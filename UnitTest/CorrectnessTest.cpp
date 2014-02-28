bd28bea018fbe5fa54eda1808e2469ee21a65902
aedampir@gmail.com <aedampir@gmail.com>
2014-02-28 09:20:09 -0800

    Minor changes

--- UnitTest/CorrectnessTest.cpp
+++ UnitTest/CorrectnessTest.cpp
@@ -7,7 +7,7 @@
 #ifndef COMMON_H_INCLUDED
 #include "Common.h"
 #endif
-#define FULL_TEST false
+#define FULL_TEST true
 
 class TestBase : public testing::Test
 {
@@ -190,50 +190,6 @@ protected:
 	virtual ~GpuTest()
 	{
 	}
-
-
-
-	double get_cpu_integr_result_internal(ComputeParameters p, double* rhoInPrevTL_asV)
-	{
-		//double buf_D = integUnderUnunifTr(
-		//	p.a, p.b,
-		//	//
-		//	p.lb, p.rb,
-		//	//
-		//	p.bb, p.ub,
-		//	//
-		//	p.tau, p.currentTimeLevel,
-		//	//
-		//	t1.first, t1.second, t1.third,
-		//	//
-		//	p.x, p.x_size,
-		//	//
-		//	p.y, p.y_size,
-		//	rhoInPrevTL_asV,
-		//	p.i, p.j);
-
-
-		//buf_D += integUnderUnunifTr(
-		//	p.a, p.b, //   -  Analitycal solution parameters.
-		//	//
-		//	p.lb, p.rb, //   -  Left and right boundaries of rectangular domain.
-		//	//
-		//	p.bb, p.ub, //   -  Botton and upper boundaries of rectangular domain.
-		//	//
-		//	p.tau, p.currentTimeLevel, //   -  Index of current time layer.
-		//	//
-		//	s1.first, s1.second, s1.third, //   -  Vertices of first triangle.
-		//	//
-		//	p.x, p.x_size, //   -  Number of OX steps.
-		//	//
-		//	p.y, p.y_size, //   -  Number of OY steps.
-		//	//
-		//	rhoInPrevTL_asV,
-		//	p.i, p.j);
-
-		//return buf_D;
-		return 0;
-	}
 };
 
 
@@ -300,7 +256,7 @@ TEST_F(GpuTest, get_quad_coord_te)
 	double first_x1(0), second_x1(0), third_x1(0), first_x2(0), second_x2(0), third_x2(0);
 	double first_y1(0), second_y1(0), third_y1(0), first_y2(0), second_y2(0), third_y2(0);
 	int finish_level = 9;
-	int start_level = 7;
+	int start_level = 8;
 
 
 	for (int level = start_level; level < finish_level; level++)
@@ -316,7 +272,6 @@ TEST_F(GpuTest, get_quad_coord_te)
 		time_gpu = get_quad_coord(gpu, p);
 
 		printf("End GPU\n");
-
 
 		printf("Start CPU\n");
 
@@ -347,114 +302,7 @@ TEST_F(GpuTest, get_quad_coord_te)
 		delete gpu;
 		delete p;
 	}
-	//std::cin.get();
 }
-
-
-
-
-//основной тест
-//TEST_F(GpuTest, DISABLED_d_integUnderUnunifTr)
-TEST_F(GpuTest, d_integUnderUnunifTr)
-{
-	//// параметры размерности задачи
-	//int levelStep = 1;
-	////int start_level =7;
-	//int start_level =0;
-	////int start_level =9;
-	//int finish_level = 1;
-
-	//// параметры сетки для GPU
-	//int grid_size = 512;
-	//int block_size = 1024;
-
-	//for (int level = start_level; level < finish_level; level++)
-	//{
-	//	ComputeParameters p = GetComputeParameters(level);
-	//	cout << p << std::endl;
-
-	//	for(p.currentTimeLevel = 1; p.currentTimeLevel < 2; p.currentTimeLevel += levelStep)
-	//	{
-	//		// init test data
-	//		p.print_info();
-	//		double time_cpu = -1;
-	//		double time_gpu = -1;
-	//		double* rho_in_prev_tl = new double [ p.size ];
-
-	//		//   Initial data of rho.
-	//		for( int k = 0; k <= p.x_size; k++ )
-	//		{
-	//			for( int j = 0; j <= p.y_size; j++ )
-	//			{
-	//				rho_in_prev_tl[ (p.x_size+1)*k + j ] = d_initDataOfSol(p, j, k);
-	//			}
-	//		}
-
-	//		// ==
-
-
-	//		// get gpu result of integr
-	//		printf("Start GPU triangulation...  ");
-	//		TriangleResult* gpu = new TriangleResult(p);
-	//		get_triangle_type(gpu, p, grid_size,
-	//		                  block_size);
-	//		printf("GPU triangulation is done.\n");
-
-	//		int c = -1;
-	//		double* cpu_result = new double[p.x_size * p.y_size];
-
-	//		printf("Perform CPU integration...   ");
-	//		StartTimer();
-
-	//		for (int j = 1; j < p.y_size; j++)
-	//		{
-	//			for (int i = 1; i < p.x_size; i++)
-	//			{
-	//				p.i = i;
-	//				p.j = j;
-	//				c = (p.x_size - 1) * (j - 1) + (i - 1);
-
-	//				//printf("\n i = %d, j = %d, c is = %d\n", i, j, c);
-
-	//				cpu_result[c] = get_cpu_integr_result_internal(p, rho_in_prev_tl, gpu->f[c], gpu->s[c]);
-	//			}
-	//		}
-
-	//		time_cpu = GetTimer();
-	//		printf("CPU integration is done.\n");
-
-	//		printf("Perform GPU integration...   ");
-	//		StartTimer();
-	//		double* gpu_integr = GetIntegrGpuResult(p, *gpu, rho_in_prev_tl, grid_size, block_size
-	//		                                        , p.get_chunk_size());
-	//		time_gpu = GetTimer();
-	//		printf("GPU integration is done.\n");
-
-
-	//		printf("CPU time is = %f ms.\n", time_cpu);
-	//		printf("GPU time is = %f ms.\n", time_gpu);
-	//		printf("CPU/GPU = %f\n times\n", time_cpu/time_gpu);
-
-
-	//		// проверям корректность полученных результатов
-	//		for (int j = 1; j < p.y_size; j++)
-	//		{
-	//			for (int i = 1; i < p.x_size; i++)
-	//			{
-	//				c = (p.x_size - 1) * (j - 1) + (i - 1);
-
-	//				ASSERT_DOUBLE_EQ(cpu_result[c], gpu_integr[c]);
-	//			}
-	//		}
-
-	//		delete[] cpu_result;
-	//		delete[] gpu_integr;
-	//		delete[] rho_in_prev_tl;
-	//		delete gpu;
-	//	}
-	//}
-}
-
 
 class CpuVersusGpuFunctionalFemTest : public TestBase
 {
